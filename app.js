a4e4c29330f8f8f5925c0d3968859c8982134ba0
Jesse <jester070993@gmail.com>
2019-06-29 20:49:53 -0700

    starter files

---
+++ app.js
@@ -0,0 +1,14 @@
+let d = new Date();
+d = d.toLocaleString();
+let hours = d.getHours();
+let minutes = d.getMinutes();
+let sec = d.getSeconds();
+
+// if hours is greater or equal to 12, "end" variable is  equal to pm, if less than 12 "end" is AM
+
+//if hours is greater than 12, do hours minus 12 to get time in PM, else keep hours as is
+
+//if hours is 00 then hours is equal to 12
+
+//if hours is less than or 1
+
